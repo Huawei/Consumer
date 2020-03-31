de5395851da00d6c74c22fbd21ee71ef9a97029c
wangxihai <45583027+wangxihai@users.noreply.github.com>
2020-03-31 21:27:15 +0800

    Scan Kit sample updated.
    Updated scan kit SDK 1.1.3.300

--- Codelabs/ScanKit-CustomizedView/app/src/main/java/com/example/scankitdemo/DefinedActivity.java
+++ Codelabs/ScanKit-CustomizedView/app/src/main/java/com/example/scankitdemo/DefinedActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -20,13 +21,17 @@ import com.huawei.hms.ml.scan.HmsScan;
 
 public class DefinedActivity extends Activity {
 
-    private static final String TAG = "DefinedActivity";
+    private static final String TAG = "DefinedActivity2";
 
     //declare RemoteView instance
     private RemoteView remoteView;
     //declare the key ,used to get the value returned from scankit
     public static final String SCAN_RESULT = "scanResult";
 
+    int mScreenWidth;
+    int mScreenHeight;
+    //scan_view_finder width & height is  300dp
+    final int SCAN_FRAME_SIZE = 300;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +39,24 @@ public class DefinedActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_defined);
 
-        //set scanning area
+
+        //1.get screen density to caculate viewfinder's rect
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        float density = dm.density;
+        //2.get screen size
+        mScreenWidth = getResources().getDisplayMetrics().widthPixels;
+        mScreenHeight = getResources().getDisplayMetrics().heightPixels;
+
+        int scanFrameSize = (int) (SCAN_FRAME_SIZE * density);
+
+        //3.caculate viewfinder's rect,it's in the middle of the layout
+        //set scanning area(Optional, rect can be null,If not configure,default is in the center of layout)
         Rect rect = new Rect();
-        rect.top = 100;
-        rect.bottom = 2000;
+        rect.left = mScreenWidth / 2 - scanFrameSize / 2;
+        rect.right = mScreenWidth / 2 + scanFrameSize / 2;
+        rect.top = mScreenHeight / 2 - scanFrameSize / 2;
+        rect.bottom = mScreenHeight / 2 + scanFrameSize / 2;
+
 
         //initialize RemoteView instance, and set calling back for scanning result
         remoteView = new RemoteView.Builder().setContext(this).setBoundingBox(rect).setFormat(HmsScan.ALL_SCAN_TYPE).build();
